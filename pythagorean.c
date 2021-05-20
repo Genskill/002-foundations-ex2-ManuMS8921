1daf37d0d405ed7688b58b028eeaa054b582c516
ManuMS8921 <84288600+ManuMS8921@users.noreply.github.com>
2021-05-20 15:49:34 +0530

    Add files via upload

--- pythagorean.c
+++ pythagorean.c
@@ -1,19 +1,27 @@
-#include<stdio.h>
-int main()
-{
-	float k,n,m,i;
-	
-    
-    float n = get_float("Enter the number");
-    float m = get_float("Enter the number");
-    float k = get_float("Enter the number");
-    
-    i=n*n+m*m;
-    if(i==k*k)
-    printf("YES,Sides of right angled triangle");
-    else
-    printf("FAIL,not a sides of right angled triangle");
-
-	
-	return 0;
+#include <stdio.h> 
+#include <cs50.h>
+int main() 
+{float max,m,n; 
+	printf("Enter 3 numbers \n"); 
+	float a= get_float("side 1 %0.0f \n",a);  
+	float b= get_float("side 2 %0.0f \n",b); 
+	float c= get_float("side 3 %0.0f \n",c); 
+	if(a>b&&a>c) 
+	{max=a;
+	m=b; 
+	n=c;} 
+	else if(b>c) 
+	{max=b;
+	m=a; 
+	n=c;} 
+	else  
+	{max=c; 
+	 m=a; 
+	 n=b;}
+if(max*max== m*m+n*n)
+     printf("Yes"); 
+	 else 
+	 printf("No"); 
+	 	
 }
+
