41f84aaf146a389c8378c3bc5f91f8a1f34fb14b
ManuMS8921 <84288600+ManuMS8921@users.noreply.github.com>
2021-05-20 15:43:43 +0530

    Add files via upload

--- pattern.c
+++ pattern.c
@@ -2,33 +2,35 @@
 #include<cs50.h>
 int main(void)
 {
-	int i,j,k,n,m;
-	
-    
-    int m = get_int("Which type of pattern where you can enter 1 or 2 ");
-    int n = get_int("Enter the row");
-	
-	
-	
-	if(m==1)
+	int type = get_int("Enter type of pattern (1 or 2) ");
+	int num_rows = get_int("Number of rows");
+	int c=num_rows;
+	switch(type)
 	{
-    	for(i=1;i<=n;i++)
-    	{
-    		for(j=i;j<n;j++)
-    		printf(" ");
-    		 	for(k=1;k<=i;k++)
-    			 	printf("*");
-    	printf("\n");		 	
-    	}
+		case 1: for(int i=1;i<=num_rows;i++)
+			{
+				for(int j=num_rows;j>=i;j--)
+				{
+					printf("#");
+				}
+				printf("\n");
+			}
+			break;
+ 
+		case 2: for(int i=1;i<=num_rows;i++)
+			{
+				for(int k=i;k<num_rows;k++)
+				{
+					printf(" ");
+				}
+				for(int j=1;j<=i;j++)
+				{
+					printf("#");
+				}
+				printf("\n");
+			}
+			break;
+ 
+		default: break;
 	}
-	else
-	{
-    	for(i=n;i>=1;i--)
-    	{
-    	    for(j=1;j<=i;j++)
-    	        printf("*");
-    	 printf("\n");                           
-    	}
-	}	
-	return 0;
 }
