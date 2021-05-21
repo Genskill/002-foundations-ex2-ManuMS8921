08af37cf7ae6391315acefb6d9f1b4b709e95ad4
ManuMS8921 <84288600+ManuMS8921@users.noreply.github.com>
2021-05-21 15:16:59 +0530

    Add files via upload

--- pythagorean.c
+++ pythagorean.c
@@ -1,27 +1,40 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <cs50.h>
-int main() 
-{float max,m,n; 
-	printf("Enter 3 numbers \n"); 
-	float a= get_float("side 1 %0.0f \n",a);  
-	float b= get_float("side 2 %0.0f \n",b); 
-	float c= get_float("side 3 %0.0f \n",c); 
-	if(a>b&&a>c) 
-	{max=a;
-	m=b; 
-	n=c;} 
-	else if(b>c) 
-	{max=b;
-	m=a; 
-	n=c;} 
-	else  
-	{max=c; 
-	 m=a; 
-	 n=b;}
-if(max*max== m*m+n*n)
-     printf("Yes"); 
-	 else 
-	 printf("No"); 
-	 	
-}
 
+int main(){
+    float a,b,c,small_1,small_2,large;
+
+    a = get_float("side 1 ");
+    b = get_float("side 2 ");
+    c = get_float("side 3 ");
+    if (a<b){
+        if (b<c){
+            small_1 = a;
+            small_2 = b;
+            large = c;
+        }else{
+            small_1 = a;
+            small_2 = c;
+            large = b;
+        }
+    }
+    else{
+        if (a<c){
+            small_1 = b;
+            small_2 = a;
+            large = c;
+        }
+        else{
+            small_1 = b;
+            small_2 = c;
+            large = a;
+        }
+
+    }
+    if (small_1*small_1 + small_2*small_2 == large*large){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
